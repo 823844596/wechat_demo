f837a80d63e3d46b8b58d7bab40ba4850689b78b
823844596 <823844596@qq.com>
2018-05-10 21:04:30 +0800

    movable-view

---
+++ wechat_demo1/viewComponent/movable-view/movableView.js
@@ -0,0 +1,23 @@
+Page({
+  data: {
+    x: 0,
+    y: 0
+  },
+
+  tap: function (e) {
+    this.setData({
+      x: 30,
+      y: 30
+    })
+  },
+
+  onChange: function (e) {
+    console.log(e, 'this is touch change');
+    console.log(e.detail);
+  },
+
+  onScale: function (e) {
+    console.log('this is scale change');
+    console.log(e.detail)
+  }
+})
