45b6aac6bc1750e63d18db734893560e9422acb3
823844596 <823844596@qq.com>
2018-05-12 10:29:00 +0800

    add text

---
+++ wechat_demo1/components-content/text/text.js
@@ -0,0 +1,8 @@
+const selectableData = '这是文本组件，设置selectable=true，支持选中效果'
+const spaceData = '这是通过设置decode=true，支持解码，使用转译符&nbsp;&nbsp;设置的空格数据'
+Page({
+  data: {
+    selectableData: selectableData,
+    spaceData: spaceData
+  }
+})
